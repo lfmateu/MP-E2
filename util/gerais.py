738ac3bf14561cdddf0094d380ba0eabaebd41b9
Luís Felipe Mateu Silva <167997374+lfmateu@users.noreply.github.com>
2026-04-06 16:47:39 -0400

    Delete util directory

--- util/gerais.py
+++
@@ -1,5 +0,0 @@
-def imprimir_objetos(cabeçalho, objetos):
-    print('\n' + cabeçalho)
-    for índice, objeto in enumerate(objetos):
-        formato = '{:<5} {}'
-        print(formato.format(str(índice + 1) + ' - ', str(objeto)))
